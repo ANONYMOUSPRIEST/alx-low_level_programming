bba91f6e8c109cb1583636198297e350446fa82e
ANONYMOUSPRIEST <richardasuquojames@gmail.com>
2022-09-10 00:32:23 -0700

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - print if the number is positive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+int c;
+int d = 0;
+while (d < 10)
+{
+c = 0;
+if (d != c && d < c)
+{
+	putchar('0' + d);
+	putchar('0' + c);
+if ( c + d != 17)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
