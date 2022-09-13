8905d22e92b9d833bea1e7e75570c22cac532dd8
ANONYMOUSPRIEST <richardasuquojames@gmail.com>
2022-09-13 11:32:14 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b =0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			product = a * b;
 			if (b != 0)
@@ -18,7 +18,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if ( b == 0)
+			if (b == 0)
 			{
 				_putchar('0');
 			}
@@ -29,10 +29,10 @@ void times_table(void)
 			}
 			else if ((product < 10) && (b != 0))
 			{
-			        _putchar(' ');
-			        _putchar((product % 10) + '0');
+				_putchar(' ');
+				_putchar((product % 10) + '0');
 			}
 		}
 	        _putchar('\n');
 	}
-}	
+}
