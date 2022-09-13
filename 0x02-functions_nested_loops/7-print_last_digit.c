644f86e3c5b19b3d079cb0b8650063c80cd29108
ANONYMOUSPRIEST <richardasuquojames@gmail.com>
2022-09-13 10:00:28 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  *print_last_digit - Prints the last digit of a number.
- *@n: the number in question.
+ *@n: The number in question.
  *
  *Return: Value of the last digit.
  */
@@ -12,8 +12,8 @@ int print_last_digit(int n)
 
 	if (last_digit < 10)
 		last_digit *= -1;
-
-	_putchar(last_digit + '0');
+        else
+		_putchar(last_digit + '0');
 
 	return (last_digit);
 }
