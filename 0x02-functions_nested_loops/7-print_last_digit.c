4ac2ecf965f477bc74f81f3869915b1624c6874a
ANONYMOUSPRIEST <richardasuquojames@gmail.com>
2022-09-13 10:06:12 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
 
-	if (last_digit < 10)
-		last_digit *= -1;
-        else
-		_putchar(last_digit + '0');
+	if (last_digit < 0)
+		last_digit = -1 * (n % 10);
+	else
+		last_digit = n % 10;
 
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
