8d8f9f51f7ac7e4c5f0e8556f798885187008da2
ANONYMOUSPRIEST <richardasuquojames@gmail.com>
2022-09-30 14:25:20 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,10 +6,11 @@
  * @argv: This is the argument vector
  *
  * Return: This return to 0
+ *
  */
 int main(int args, char *argv[])
 {
 	printf("%d\n", args - 1);
-	(void)argv
-		return (0);
+	(void)argv;
+	return (0);
 }
